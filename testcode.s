497a186db2392acc7e6596ef9cc2f8773b48bf39
Aditya Rajagopal <Aditya@adityarjagopal3.home>
2016-03-15 12:21:16 +0000

    updated test

--- testcode.s
+++ testcode.s
@@ -4,8 +4,8 @@
 	.ent	f
 	.type	f, @function
 f:
-addiu	$sp,$sp,-24
-sw	$fp,20($sp)
+addiu	$sp,$sp,-32
+sw	$fp,28($sp)
 move	$fp,$sp
 lw	$8,12($fp)
 move	$8,$4
@@ -24,8 +24,8 @@ mflo	$8
 sw	$8,20($fp)
 lw	$2,20($fp)
 move	$sp,$fp
-lw	$fp,20($sp)
-addiu	$sp,$sp,24
+lw	$fp,28($sp)
+addiu	$sp,$sp,32
 j	$31
 nop
 	.end	f
