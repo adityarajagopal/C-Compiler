1e56595e79a39da9ba6d5503bf814ba463b3c31d
Aditya Rajagopal <Aditya@adityarjagopal3.home>
2016-03-15 10:52:53 +0000

    updated test

--- testcode.s
+++ testcode.s
@@ -1,44 +1,24 @@
 	.text
 	.align	2
-	.globl	main
-	.type	main, @function
-main:
-addiu	$sp,$sp,-40
-sw	$fp,36($sp)
+	.globl	f
+	.type	f, @function
+f:
+addiu	$sp,$sp,-24
+sw	$fp,20($sp)
 move	$fp,$sp
-lw	$8,12($fp)
-move	$8,$4
-sw	$8,12($fp)
-lw	$8,16($fp)
-move	$8,$5
-sw	$8,16($fp)
-lw	$8,12($fp)
-sw	$8,12($fp)
-lw	$8,24($fp)
-li	$8,3
-sw	$8,24($fp)
-lw	$8,24($fp)
-sw	$8,12($fp)
 lw	$8,16($fp)
 sw	$8,16($fp)
-lw	$8,32($fp)
-li	$8,4
-sw	$8,32($fp)
-lw	$8,32($fp)
-sw	$8,16($fp)
-lw	$8,12($fp)
-sw	$8,12($fp)
+lw	$8,20($fp)
+sw	$8,20($fp)
 lw	$8,16($fp)
-sw	$8,16($fp)
-lw	$8,12($fp)
-lw	$9,16($fp)
+lw	$9,20($fp)
 mult	$8,$9
 mflo	$8
-sw	$8,36($fp)
-lw	$2,36($fp)
+sw	$8,12($fp)
+lw	$2,12($fp)
 move	$sp,$fp
-lw	$fp,36($sp)
-addiu	$sp,$sp,40
+lw	$fp,20($sp)
+addiu	$sp,$sp,24
 j	$31
 nop
 
