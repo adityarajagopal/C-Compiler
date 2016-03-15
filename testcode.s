dca68eb6f188b7c44466cd1f54a4f05bc429510d
Aditya Rajagopal <Aditya@adityarjagopal3.home>
2016-03-15 10:36:41 +0000

    updated test

--- testcode.s
+++ testcode.s
@@ -3,8 +3,8 @@
 	.globl	f
 	.type	f, @function
 f:
-addiu	$sp,$sp,-24
-sw	$fp,20($sp)
+addiu	$sp,$sp,-40
+sw	$fp,36($sp)
 move	$fp,$sp
 lw	$8,12($fp)
 move	$8,$4
@@ -14,17 +14,31 @@ move	$8,$5
 sw	$8,16($fp)
 lw	$8,12($fp)
 sw	$8,12($fp)
+lw	$8,24($fp)
+li	$8,3
+sw	$8,24($fp)
+lw	$8,24($fp)
+sw	$8,12($fp)
+lw	$8,16($fp)
+sw	$8,16($fp)
+lw	$8,32($fp)
+li	$8,4
+sw	$8,32($fp)
+lw	$8,32($fp)
+sw	$8,16($fp)
+lw	$8,12($fp)
+sw	$8,12($fp)
 lw	$8,16($fp)
 sw	$8,16($fp)
 lw	$8,12($fp)
 lw	$9,16($fp)
 mult	$8,$9
 mflo	$8
-sw	$8,20($fp)
-lw	$2,20($fp)
+sw	$8,36($fp)
+lw	$2,36($fp)
 move	$sp,$fp
-lw	$fp,20($sp)
-addiu	$sp,$sp,24
+lw	$fp,36($sp)
+addiu	$sp,$sp,40
 j	$31
 nop
 
